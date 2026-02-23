05fd9985985fb883f3453dd3ffc124622de78736
frolleks <spaceactive.akbar@gmail.com>
2026-02-23 16:30:16 +0700

    fix: type error when using react-router-dom's NavLink in the AuthUIProvider

--- app/src/client/components/providers/index.tsx
+++ app/src/client/components/providers/index.tsx
@@ -1,13 +1,13 @@
 import { AuthUIProvider } from "@daveyplate/better-auth-ui";
 import { authClient } from "@/client/lib/auth";
-import { useNavigate, NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ThemeProvider } from "./theme-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const navigate = useNavigate();
 
   return (
-    <AuthUIProvider authClient={authClient} navigate={navigate} Link={NavLink}>
+    <AuthUIProvider authClient={authClient} navigate={navigate}>
       <ThemeProvider>{children}</ThemeProvider>
     </AuthUIProvider>
   );
