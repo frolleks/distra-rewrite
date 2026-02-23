945a469a2f4bbe2a05ab7cefc0f3f5ef3ed85ee9
frolleks <spaceactive.akbar@gmail.com>
2026-02-23 16:04:25 +0700

    chore: move the root page into its own file

--- app/src/client/App.tsx
+++ app/src/client/App.tsx
@@ -1,61 +1,14 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/client/components/ui/card";
 import "./index.css";
 
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
 import { Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/auth/page";
-import { ThemeToggle } from "./components/theme/theme-toggle";
+import RootPage from "./pages/root/page";
 
 export function App() {
   return (
     <>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <div className="container mx-auto p-8 text-center relative z-10">
-                <div className="flex justify-center items-center gap-8 mb-8">
-                  <img
-                    src={logo}
-                    alt="Bun Logo"
-                    className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-                  />
-                  <img
-                    src={reactLogo}
-                    alt="React Logo"
-                    className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] animate-[spin_20s_linear_infinite]"
-                  />
-                </div>
-                <ThemeToggle />
-                <Card>
-                  <CardHeader className="gap-4">
-                    <CardTitle className="text-3xl font-bold">
-                      Bun + React
-                    </CardTitle>
-                    <CardDescription>
-                      Edit{" "}
-                      <code className="rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono">
-                        src/App.tsx
-                      </code>{" "}
-                      and save to test HMR
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <p>hello world</p>
-                  </CardContent>
-                </Card>
-              </div>
-            </>
-          }
-        />
+        <Route path="/" element={<RootPage />} />
         <Route path="/auth/:pathname" element={<AuthPage />} />
       </Routes>
     </>
